ac66171406a301e014e0fb1a85477196d18842ff
Lapatrada2000 <71629112+Lapatrada2000@users.noreply.github.com>
2021-03-10 20:06:58 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,8 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'PZ6qlbYABvcIg+sly4KFcjs8rAVOW1+EEEDBgcOn86a9MwA+MNHV8//FPERaqcVuWnKEs4U+6oe0jLA++fQlGKdK9/SCRKlZ0x4otRbscQZBRbe5VDkXvu32iZAA+dpXEwrb47Ncr9kuH1vSp+t3LwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'XzOGm22oahF8eKhT4WK6So4JJEiNgHSEwo2MddBtMRusLc51i0hzAO2rcF5t9tJ6whir9iGNyG8rYCqvyVO0TQpOoY5XyctzrzZuA5Zw02WjnDOppWNAn7xjsCe8yfiYZYHPxLI+GL1xZmETxAspKgdB04t89/1O/w1cDnyilFU=
+'; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
